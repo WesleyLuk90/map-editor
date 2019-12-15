fb26b68dd216998be933a77821890d08c9ed487a
Wesley Luk <wesleyluk@gmail.com>
2019-12-15 11:26:39 -0800

    wip

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,8 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { isContext } from "vm";
+
+class Size {
+    constructor(
+        readonly x: number,
+        readonly y: number,
+        readonly scale: number
+    ) {}
+}
 
 export function Canvas({ image }: { image: File | null }) {
     const container = React.createRef<HTMLDivElement>();
     const ref = React.createRef<HTMLCanvasElement>();
+
+    const [size, setSize] = useState(new Size(0, 0, 1));
+
+    function render(ctx: CanvasRenderingContext2D, image: ImageBitmap) {
+        ctx.drawImage(
+            image,
+            size.x,
+            size.y,
+            image.width * size.scale,
+            image.height * size.scale
+        );
+    }
 
     useEffect(() => {
         if (!image) {
