f051654a2e03dbf4c4c4bd6b6b4ebfc76026392c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-20 07:26:14 +0000

    Deploy WesleyLuk90/map-editor to github.com/WesleyLuk90/map-editor.git:gh-pages

---
+++ precache-manifest.a111ce28f35a1c00d1b01831d59712a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75df9ed98c5cec4406dc5102a3b27df",
+    "url": "/map-editor/index.html"
+  },
+  {
+    "revision": "812c3394af6b12a989a9",
+    "url": "/map-editor/static/css/main.27b66697.chunk.css"
+  },
+  {
+    "revision": "ec8481d674eead2109ef",
+    "url": "/map-editor/static/js/2.1481248c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/map-editor/static/js/2.1481248c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "812c3394af6b12a989a9",
+    "url": "/map-editor/static/js/main.ad32b0bb.chunk.js"
+  },
+  {
+    "revision": "e134caa6c2004338c5cc",
+    "url": "/map-editor/static/js/runtime-main.635c9f42.js"
+  }
+]);
